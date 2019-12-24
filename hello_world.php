e0c40b9a2a37f9e92e61ac09534704b9d4fb81c9
kei <kei.adc11.29@gmail.com>
2019-12-24 19:10:16 +0900

    add スクレイピング by php
    任天堂のサイトに対してスクレイピングをして、見出し(h1)を表示します。

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,12 @@
 <?php
-
-
 echo "Hello world!";
 
+//phpQueryの読み込み
+require_once('./phpQuery-onefile.php');
+
+//指定したwebサイトから読み込む
+$html = file_get_contents('https://www.nintendo.com/');
+
+//結果を表示、見出しだけ
+echo phpQuery::newDocument($html)->find('h1')->text();
 ?>
