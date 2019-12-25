ff477d612f9a1261971ad4292a1c48708737638f
BuildTools <unconfigured@null.spigotmc.org>
2019-12-25 15:19:53 +0900

    末尾再帰最適化を実装

--- hello_world.php
+++ hello_world.php
@@ -10,14 +10,14 @@ $html = file_get_contents('https://www.nintendo.com/');
 //結果を表示、見出しだけ
 echo phpQuery::newDocument($html)->find('h1')->text();
 
+$arr = array();
 
-function fact($args){
-  if($args > 0){
-    return $args * fact($args - 1);
-  } 
-
-  return 1;
+function fact($i, $acc){
+  if($i == 0){ 
+    return $acc;
+  }else{
+    return fact($i-1, $i*$acc);
+  }
 }
-
-echo "\n 10の階乗 " . fact(10);//10の階乗
+echo "\n 10の階乗 " . fact(10, 1);//10の階乗
 ?>
