afe9bd4afedc8f1a2dadd5cf8bdbed1e8bf802e8
BuildTools <unconfigured@null.spigotmc.org>
2019-12-25 15:04:49 +0900

    fix b add 階乗計算

--- hello_world.php
+++ hello_world.php
@@ -9,4 +9,15 @@ $html = file_get_contents('https://www.nintendo.com/');
 
 //結果を表示、見出しだけ
 echo phpQuery::newDocument($html)->find('h1')->text();
+
+
+function fact($args){
+  if($args > 0){
+    return $args * fact($args - 1);
+  } 
+
+  return 1;
+}
+
+echo "\n 10の階乗 " . fact(10);//10の階乗
 ?>
